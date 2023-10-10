9f7f6530342852de4b59c3285c3cfecd7d1fded3
Andrei Stanciu <andreistanciu.as@gmail.com>
2023-10-10 07:55:03 +0300

    Update the platform-user config

--- flake.nix
+++ flake.nix
@@ -167,13 +167,9 @@
     darwinConfigurations = {
       "stanciua@x86_64-darwin" = mkDarwinConfig {
         system = "x86_64-darwin";
-        extraModules = [./profiles/personal.nix ./modules/darwin/apps.nix];
+        extraModules = [./profiles/work.nix ./profiles/personal.nix ./modules/darwin/apps.nix];
       };
       "stanciua@aarch64-darwin" = mkDarwinConfig {
-        system = "aarch64-darwin";
-        extraModules = [./profiles/work.nix];
-      };
-      "stanciua@x86_64-darwin" = mkDarwinConfig {
         system = "aarch64-darwin";
         extraModules = [./profiles/work.nix];
       };
